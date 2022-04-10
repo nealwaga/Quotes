4c3288a924093e123335fc39eaa0d5c92f9f445d
nealwaga <waganeal@gmail.com>
2022-04-10 18:29:05 +0300

    quotes component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,11 +8,14 @@ import { Quote } from '../quote';
 })
 export class QuotesComponent implements OnInit {
 
-  quotes: Quote[] = [
+  public quotes: Quote[] = [
     new Quote (1, 'First Quote', 0, 0),
     new Quote (2, 'Second Quote', 0, 0),
     new Quote (3, 'Third Quote', 0, 0)
   ]
+
+  arr: number [] = this.quotes.map(quote=>quote.upvotes)
+  highest = Math.max (...this.arr)
 
   constructor() { }
 
